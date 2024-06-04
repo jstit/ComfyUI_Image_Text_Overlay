1a6769ae0c28cf9d5049319a91e206d1108cec6e
jstit <trantuancn17a@gmail.com>
2024-06-04 16:08:55 +0700

    mod: custom fonts

--- ImageTextOverlay.py
+++ ImageTextOverlay.py
@@ -2,10 +2,13 @@ from PIL import Image, ImageDraw, ImageFont
 from urllib.request import urlopen
 import torch
 import numpy as np
+import os
+
 
 class ImageTextOverlay:
     def __init__(self, device="cpu"):
         self.device = device
+
     _alignments = ["left", "right", "center"]
 
     @classmethod
@@ -14,13 +17,19 @@ class ImageTextOverlay:
             "required": {
                 "image": ("IMAGE",),
                 "text": ("STRING", {"multiline": True, "default": "Hello"}),
-                "textbox_width": ("INT", {"default": 200, "min": 1}),  
-                "textbox_height": ("INT", {"default": 200, "min": 1}),  
-                "max_font_size": ("INT", {"default": 30, "min": 1, "max": 256, "step": 1}),  
-                "font": ("STRING", {"default": "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"}), 
-                "alignment": (cls._alignments, {"default": "center"}),  
-                "color": ("STRING", {"default": "#000000"}),  
-                "start_x": ("INT", {"default": 0}),  
+                "textbox_width": ("INT", {"default": 200, "min": 1}),
+                "textbox_height": ("INT", {"default": 200, "min": 1}),
+                "max_font_size": (
+                    "INT",
+                    {"default": 30, "min": 1, "max": 2560, "step": 1},
+                ),
+                "font": (
+                    "STRING",
+                    {"default": "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"},
+                ),
+                "alignment": (cls._alignments, {"default": "center"}),
+                "color": ("STRING", {"default": "#000000"}),
+                "start_x": ("INT", {"default": 0}),
                 "start_y": ("INT", {"default": 0}),
                 "padding": ("INT", {"default": 50}),
                 "line_height": ("INT", {"default": 20, "min": 1}),
@@ -34,15 +43,15 @@ class ImageTextOverlay:
     def wrap_text_and_calculate_height(self, text, font, max_width, line_height):
         wrapped_lines = []
         # Split the input text by newline characters to respect manual line breaks
-        paragraphs = text.split('\n')
-        
+        paragraphs = text.split("\n")
+
         for paragraph in paragraphs:
             words = paragraph.split()
-            current_line = words[0] if words else ''
-            
+            current_line = words[0] if words else ""
+
             for word in words[1:]:
                 # Test if adding a new word exceeds the max width
-                test_line = current_line + ' ' + word if current_line else word
+                test_line = current_line + " " + word if current_line else word
                 test_line_bbox = font.getbbox(test_line)
                 w = test_line_bbox[2] - test_line_bbox[0]  # Right - Left for width
                 if w <= max_width:
@@ -51,35 +60,67 @@ class ImageTextOverlay:
                     # If the current line plus the new word exceeds max width, wrap it
                     wrapped_lines.append(current_line)
                     current_line = word
-            
+
             # Don't forget to add the last line of the paragraph
             wrapped_lines.append(current_line)
 
         # Calculate the total height considering the custom line height
         total_height = len(wrapped_lines) * line_height
 
-        wrapped_text = '\n'.join(wrapped_lines)
+        wrapped_text = "\n".join(wrapped_lines)
         return wrapped_text, total_height
 
-
-    def add_text_overlay(self, image, text, textbox_width, textbox_height, max_font_size, font, alignment, color, start_x, start_y, padding, line_height):
+    def add_text_overlay(
+        self,
+        image,
+        text,
+        textbox_width,
+        textbox_height,
+        max_font_size,
+        font,
+        alignment,
+        color,
+        start_x,
+        start_y,
+        padding,
+        line_height,
+    ):
         image_tensor = image
         image_np = image_tensor.cpu().numpy()
         image_pil = Image.fromarray((image_np.squeeze(0) * 255).astype(np.uint8))
-        color_rgb = tuple(int(color.lstrip("#")[i:i+2], 16) for i in (0, 2, 4))
+        color_rgb = tuple(int(color.lstrip("#")[i : i + 2], 16) for i in (0, 2, 4))
 
         effective_textbox_width = textbox_width - 2 * padding  # Adjust for padding
         effective_textbox_height = textbox_height - 2 * padding
 
         font_size = max_font_size
         while font_size >= 1:
-            loaded_font = ImageFont.truetype(urlopen(font), font_size) if font.startswith("http") else ImageFont.truetype(font, font_size)
-            wrapped_text, total_text_height = self.wrap_text_and_calculate_height(text, loaded_font, effective_textbox_width, line_height)
+            fonts_dir = os.path.join(os.path.dirname(__file__), "fonts")
+            font_path = os.path.join(fonts_dir, font)
+
+            # Check if the font file exists in the fonts directory
+            if not os.path.exists(font_path):
+                # If not, set path to font name directly - this will cause PIL to search for the font in the system
+                font_path = font
+
+            try:
+                loaded_font = ImageFont.truetype(font_path, font_size)
+            except Exception as e:
+                print(f"Error loading font: {e}... Using default font")
+                loaded_font = ImageFont.load_default(font_size)
+
+            wrapped_text, total_text_height = self.wrap_text_and_calculate_height(
+                text, loaded_font, effective_textbox_width, line_height
+            )
 
             if total_text_height <= effective_textbox_height:
                 draw = ImageDraw.Draw(image_pil)
-                lines = wrapped_text.split('\n')
-                y = start_y + padding + (effective_textbox_height - total_text_height) // 2
+                lines = wrapped_text.split("\n")
+                y = (
+                    start_y
+                    + padding
+                    + (effective_textbox_height - total_text_height) // 2
+                )
 
                 for line in lines:
                     line_bbox = loaded_font.getbbox(line)
@@ -90,7 +131,11 @@ class ImageTextOverlay:
                     elif alignment == "right":
                         x = start_x + effective_textbox_width - line_width + padding
                     elif alignment == "center":
-                        x = start_x + padding + (effective_textbox_width - line_width) // 2
+                        x = (
+                            start_x
+                            + padding
+                            + (effective_textbox_width - line_width) // 2
+                        )
 
                     draw.text((x, y), line, fill=color_rgb, font=loaded_font)
                     y += line_height  # Use custom line height for spacing
@@ -102,6 +147,7 @@ class ImageTextOverlay:
         image_tensor_out = torch.tensor(np.array(image_pil).astype(np.float32) / 255.0)
         image_tensor_out = torch.unsqueeze(image_tensor_out, 0)
         return (image_tensor_out,)
+
 
 NODE_CLASS_MAPPINGS = {
     "Image Text Overlay": ImageTextOverlay,
